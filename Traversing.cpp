0a6a947c0834843fd0be7aea31fc2b2dad0822c9
Sanskruti Maharana <93183819+sanskrutimaharana17@users.noreply.github.com>
2022-02-21 07:58:22 +0530

    Add files via upload

---
+++ Traversing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>   
+using namespace std;
+
+int main()
+{
+	int arr[100];
+	int n;
+	cout<<"size of array";
+	cin>>n;
+	
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+    return 0;
+}
+
+																											                                                                                                             
